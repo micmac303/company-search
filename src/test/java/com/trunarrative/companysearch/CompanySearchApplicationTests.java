72c7f3a57e9b10ba534beeb982c608feaa503d04
Michael McCarthy <michaelmccarthy@Michaels-MacBook-Air.local>
2024-05-23 17:24:48 -0300

    Initial commit

---
+++ src/test/java/com/trunarrative/companysearch/CompanySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trunarrative.companysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
